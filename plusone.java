953912f0905d179f492d5bc96b23fc1abde1b2f7
san209 <survin0902@gmail.com>
2024-12-05 19:59:14 +0000

    Plus One

--- plusone.java
+++ plusone.java
@@ -1,18 +1,24 @@
 class plusone {
     public static void main(String args[]) {
-        int digits[] = {4,3,2,1};
+        int digits[] = {4,3,2,9};
         int n = digits.length;
         int digits2[] = new int[n];
         int digits3[] = {1,0};
         int num=0;
 
-        if(n == 1 && digits[0] == 9) {
-            for(int i=0; i<2; i++) {
-                System.out.print(digits3[i]+"\t");
+        if(digits[n-1] == 9 && n!=1) {
+            for(int i=0; i<n; i++) {
+                digits2[i] = digits[i];
+            }
+            digits2[n-2] = digits[n-2] + 1;
+            digits2[n-1] = 0;
+
+            for(int i=0; i<n; i++) {
+                System.out.print(digits2[i]+"\t");
             }
             System.out.println();
         }
-        else {
+        else if(digits[n-1] != 9) {
             for(int i=0; i<n; i++) {
                 if(i == n-1) {
                     digits2[i] = digits[i] + 1;
@@ -23,6 +29,12 @@ class plusone {
             }
             for(int i=0; i<n; i++) {
                 System.out.print(digits2[i]+"\t");
+            }
+            System.out.println();
+        }
+        else if(digits[n-1] == 9 && n==1) {
+            for(int i=0; i<2; i++) {
+                System.out.print(digits3[i]+"\t");
             }
             System.out.println();
         }
