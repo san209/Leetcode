45c1992133f92d739dca4059b9c0ca1bf121313f
san209 <survin0902@gmail.com>
2024-12-06 14:52:16 +0000

    Plus One

--- plusone.java
+++ plusone.java
@@ -1,19 +1,26 @@
 class plusone {
     public static void main(String args[]) {
-        int digits[] = {4,3,2,9};
+        int digits[] = {1,2,3,9};
         int n = digits.length;
-        int digits2[] = new int[n];
-        int digits3[] = {1,0};
+        int digits2[] = new int[n+1];
+        int digits3[] = new int[n];
         int num=0;
-
-        if(digits[n-1] == 9 && n!=1) {
-            for(int i=0; i<n; i++) {
-                digits2[i] = digits[i];
+        int flag=0;
+        
+        for(int i=0; i<n; i++) {
+            if(digits[i] == 9) {
+                flag++;
             }
-            digits2[n-2] = digits[n-2] + 1;
-            digits2[n-1] = 0;
+        }
+        
+        if(flag == n) {
+            for(int i=1; i<n+1; i++) {
+                digits2[i] = 0;
+            }
+            digits2[0] = 1;
+            digits2[n] = 0;
 
-            for(int i=0; i<n; i++) {
+            for(int i=0; i<n+1; i++) {
                 System.out.print(digits2[i]+"\t");
             }
             System.out.println();
@@ -32,8 +39,19 @@ class plusone {
             }
             System.out.println();
         }
-        else if(digits[n-1] == 9 && n==1) {
-            for(int i=0; i<2; i++) {
+        else if(digits[n-1] == 9 && flag != n) {
+            for(int i=0; i<n; i++) {
+                if(i == n-2) {
+                    digits3[i] = digits[i] + 1;
+                }
+                else if(i == n-1) {
+                    digits3[i] = 0;
+                }
+                else {
+                    digits3[i] = digits[i];
+                }
+            }
+            for(int i=0; i<n; i++) {
                 System.out.print(digits3[i]+"\t");
             }
             System.out.println();
