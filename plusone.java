632e5fb37fcbb3e89d0ed9a1ddc17c07886cf3b3
san209 <survin0902@gmail.com>
2024-12-06 16:58:12 +0000

    Plus One

--- plusone.java
+++ plusone.java
@@ -1,61 +1,33 @@
 class plusone {
     public static void main(String args[]) {
-        int digits[] = {1,2,3,9};
+        int digits[] = {9,9,9,9};
         int n = digits.length;
-        int digits2[] = new int[n+1];
-        int digits3[] = new int[n];
-        int num=0;
-        int flag=0;
+        int num =0;
+        int m=0;
         
         for(int i=0; i<n; i++) {
-            if(digits[i] == 9) {
-                flag++;
-            }
+            num = num*10 + digits[i];
         }
-        
-        if(flag == n) {
-            for(int i=1; i<n+1; i++) {
-                digits2[i] = 0;
-            }
-            digits2[0] = 1;
-            digits2[n] = 0;
+        num++;
+        System.out.println(num);
+        int temp = num;
 
-            for(int i=0; i<n+1; i++) {
-                System.out.print(digits2[i]+"\t");
-            }
-            System.out.println();
+        while(num != 0) {
+            num/=10;
+            m++;
         }
-        else if(digits[n-1] != 9) {
-            for(int i=0; i<n; i++) {
-                if(i == n-1) {
-                    digits2[i] = digits[i] + 1;
-                }
-                else {
-                digits2[i] = digits[i];
-                }
-            }
-            for(int i=0; i<n; i++) {
-                System.out.print(digits2[i]+"\t");
-            }
-            System.out.println();
+
+        int digits2[] = new int[m];
+
+        for(int i=m-1; i>=0 && temp != 0; i--) {
+            digits2[i] = temp%10;
+            temp /= 10;
         }
-        else if(digits[n-1] == 9 && flag != n) {
-            for(int i=0; i<n; i++) {
-                if(i == n-2) {
-                    digits3[i] = digits[i] + 1;
-                }
-                else if(i == n-1) {
-                    digits3[i] = 0;
-                }
-                else {
-                    digits3[i] = digits[i];
-                }
-            }
-            for(int i=0; i<n; i++) {
-                System.out.print(digits3[i]+"\t");
-            }
-            System.out.println();
+
+        for(int i=0; i<m; i++) {
+            System.out.print(digits2[i]+"\t");
         }
+        System.out.println();
 
     }
 }
