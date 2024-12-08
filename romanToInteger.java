8e10c136bce984616efe93745301001bfb59023a
san209 <survin0902@gmail.com>
2024-12-08 11:31:07 +0000

    Roman To Integer

--- romanToInteger.java
+++ romanToInteger.java
@@ -1,30 +1,41 @@
 class romanToInteger {
     public static void main(String args[]) {
-        String s = "XXVII";
+        String s = "MCMXCIV";
         int n = s.length();
+        int arr[] = new int[n];
         int num = 0;
 
         for(int i=0; i<n; i++) {
             if(s.charAt(i) == 'I') {
-                num += 1;
+                arr[i] = 1;
             }
             else if(s.charAt(i) == 'V') {
-                num += 5;
+                arr[i] = 5;
             }
             else if(s.charAt(i) == 'X') {
-                num += 10;
+                arr[i] = 10;
             }
             else if(s.charAt(i) == 'L') {
-                num += 50;
+                arr[i] = 50;
             }
             else if(s.charAt(i) == 'C') {
-                num += 100;
+                arr[i] = 100;
             }
             else if(s.charAt(i) == 'D') {
-                num += 500;
+                arr[i] = 500;
             }
             else if(s.charAt(i) == 'M') {
-                num += 1000;
+                arr[i] = 1000;
+            }
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(arr[i] < arr[i+1]) {
+                num = num + arr[i+1] - arr[i];
+                i++;
+            }
+            else {
+                num = num + arr[i];
             }
         }
         System.out.println(num);
